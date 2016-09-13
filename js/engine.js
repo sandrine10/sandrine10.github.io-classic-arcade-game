a1dfcbf214600d003804eaa8775fd4a033f4b4a4
Sandy O <sandrine10@users.noreply.github.com>
2016-09-13 17:36:23 -0400

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -227,7 +227,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/char-boy.png',
-        'images/Heart.png',
+        'images/heart.png',
         'images/Star.png',
         'images/Selector.png',
         'images/stone-block-highlight.png',
